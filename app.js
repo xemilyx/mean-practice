3eb901c8a33f176cffbc2ceec2f6704d189afb7d
xemilyx <emilybrown@cranstonlibrary.org>
2017-10-29 14:04:55 -0400

    Added app.use to get static files

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', process.env.PORT);
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
     console.log("GET the homepage");
