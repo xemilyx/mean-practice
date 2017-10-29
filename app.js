779e92840289216c1cecabba2bc6f1436a95b1b2
xemilyx <emilybrown@cranstonlibrary.org>
2017-10-29 14:29:59 -0400

    added middleware (in correct order

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 
 app.set('port', 3000);
 
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
